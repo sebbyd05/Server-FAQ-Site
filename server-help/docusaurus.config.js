ec0ba5497a6b6680d75830431ecc14cbb7ce4841
Sebastian Dziedzic <sebbyd05@gmail.com>
2026-09-02 22:48:48 -0700

    Fixed domain

--- server-help/docusaurus.config.js
+++ server-help/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://sebbyd05.github.com/',
+  url: 'https://sebbyd05.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Server-FAQ-Site/',
